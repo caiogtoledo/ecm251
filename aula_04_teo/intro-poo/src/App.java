70c70d227863d9f93e7497e05d0d7bd2a12427b0
Caio Toledo <caio.detoledo15@gmail.com>
2022-03-09 11:21:34 -0300

    criando classe caneta

---
+++ aula_04_teo/intro-poo/src/App.java
@@ -0,0 +1,15 @@
+
+public class App {
+    public static void main(String[] args) {
+        //Declarando e Instancia um objeto Caneta
+        Caneta c1 = new Caneta();
+        c1.modelo = "BIC";
+        c1.cor = "Azul";
+        c1.carga = 100;
+        c1.ponta = 1.0;
+        System.out.println("Minha Caneta: " + c1.modelo);
+        System.out.println("Minha Caneta: " + c1.cor);
+        System.out.println("Minha Caneta: " + c1.carga);
+        System.out.println("Minha Caneta: " + c1.ponta);
+    }
+}
---
+++ aula_04_teo/intro-poo/src/Caneta.java
@@ -0,0 +1,12 @@
+public class Caneta{
+    // Atributos
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+
+    //Métodos
+    void escrever(String texto){
+        System.out.println(texto);
+    }
+}
