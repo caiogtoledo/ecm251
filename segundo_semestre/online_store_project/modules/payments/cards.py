258c6c6ac3423c00c25aa6e50b343be2c358007c
Caio <ctoledo@pieracciani.com.br>
2022-08-18 11:03:16 -0300

    adicionando cartão de debito e crédito

--- segundo_semestre/online_store_project/modules/payments/cards.py
+++ segundo_semestre/online_store_project/modules/payments/cards.py
@@ -2,10 +2,11 @@ from payment import Payment
 
 
 class Card(Payment):
-    def __init__(self, number, holder, due_date) -> None:
+    def __init__(self, number, holder, due_date, cvv) -> None:
         self._number = number
         self._holder = holder
         self._due_date = due_date
+        self._cvv = cvv
 
     def get_holder(self):
         return self._holder
@@ -15,3 +16,19 @@ class Card(Payment):
 
     def get_due_date(self):
         return self._due_date
+
+
+class Debt(Card):
+    def __init__(self, number, holder, due_date, cvv) -> None:
+        super().__init__(number, holder, due_date, cvv)
+
+    def pay(self):
+        return True
+
+
+class Credit(Card):
+    def __init__(self, number, holder, due_date, cvv) -> None:
+        super().__init__(number, holder, due_date, cvv)
+
+    def pay(self):
+        return True
