2a586bfb47ff7f5ff161f5c72380a3ae19d024ba
Caio <ctoledo@pieracciani.com.br>
2022-09-08 10:11:26 -0300

    interface gráfica com ttkbootstrap

--- segundo_semestre/aula_19/src/test-drive/ola-mundo.py
+++ segundo_semestre/aula_19/src/test-drive/ola-mundo.py
@@ -1,9 +1,13 @@
+from turtle import position, title
 import ttkbootstrap as ttk
 
 base = ttk.Window(
     title="Minha GUI",
-    size=(1024, 400),
-    position=(100, 100),
-    alpha=0.75,
+    size= (1024,400),
+    position= (100,100),
+    minsize= (600,300),
+    maxsize= (1800,900),
+    alpha=1.0
 )
+
 base.mainloop()
